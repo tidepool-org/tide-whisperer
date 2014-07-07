9cc5306e9b3e9fb03be8c707f0e1fa702d6abbad
Eric Tschetter <cheddar@tidepool.org>
2014-07-07 18:11:46 -0500

    Add build and start files

---
+++ config/goenv.sh
@@ -0,0 +1,3 @@
+#! /bin/bash -eu
+
+echo "Put export BLAH=blah in goenv.sh to get picked up by start.sh"
---
+++ start.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -eu
+
+if [ -r config/goenv.sh ]; then
+  . config/goenv.sh
+fi
+./tide-whisperer
