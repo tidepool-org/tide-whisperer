c3a8a0e54ee290dee873e1ad68162a03f8e94bc8
Eric Tschetter <cheddar@tidepool.org>
2014-07-08 17:10:16 -0500

    Fix up package.sh

--- package.sh
+++ package.sh
@@ -2,10 +2,13 @@
 
 export GOPATH=`pwd`
 
+PROJECT=tide-whisperer
+PACKAGE=github.com/tidepool-org/${PROJECT}
 OUTDIR=dist
-
 rm -rf ${OUTDIR}
 mkdir ${OUTDIR}
-go build -o ${OUTDIR}/tide-whisperer github.com/tidepool-org/tide-whisperer
+
+jd get ${PACKAGE} || true
+go build -o ${OUTDIR}/${PROJECT} ${PACKAGE}
 
 cp start.sh ${OUTDIR}/
