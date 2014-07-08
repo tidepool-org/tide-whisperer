34906f6506d0b4f41d35e042daae2bd33b234793
Eric Tschetter <cheddar@tidepool.org>
2014-07-08 17:51:53 -0500

    Prepare for checking in dependencies

--- package.sh
+++ package.sh
@@ -4,11 +4,11 @@ export GOPATH=`pwd`
 
 PROJECT=tide-whisperer
 PACKAGE=github.com/tidepool-org/${PROJECT}
+
 OUTDIR=dist
 rm -rf ${OUTDIR}
 mkdir ${OUTDIR}
 
-jd get ${PACKAGE} || true
 go build -o ${OUTDIR}/${PROJECT} ${PACKAGE}
 
 cp start.sh ${OUTDIR}/
