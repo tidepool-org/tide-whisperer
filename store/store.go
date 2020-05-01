1fae9cddbc95b8b3be1f9ef5af24e5ae2669514b
Todd Kazakov <todd.kazakov@gmail.com>
2020-05-01 22:07:16 +0300

    Revert debug logging

--- store/store.go
+++ store/store.go
@@ -252,10 +252,6 @@ func generateMongoQuery(p *Params) bson.M {
 		}
 	}
 
-	if p.UserID == "398d0ac771" {
-		log.Printf("%s DavesGetDeviceDataMongoQuery: %v", dataStoreAPIPrefix, groupDataQuery)
-	}
-
 	return groupDataQuery
 }
 
