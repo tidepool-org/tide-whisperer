5f7ded50ef0863ad12e73c5f0182a9936d1e1cb9
James Raby <james.raby@gmail.com>
2020-09-26 14:42:13 -0700

    convert deviceTime

--- models/base.go
+++ models/base.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"strings"
 	"time"
+	"encoding/json"
 )
 
 type Model interface {
@@ -32,7 +34,7 @@ type Base struct {
 
 	PayloadJson       string     `pg:"payload" json:"payload"`
 
-	Revision          int64   `mapstructure:"revision,omitempty" pg:"revision" json:"-"`
+	Revision          int64   `mapstructure:"revision,omitempty" pg:"revision" json:"revision"`
 }
 
 func (b *Base) GetType() string {
@@ -43,4 +45,43 @@ func (b *Base) GetUserId() string {
 	return b.UserId
 }
 
+func (b *Base) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Time              time.Time  `mapstructure:"time" pg:"time,type:timestamptz" json:"time,omitempty"`
 
+		Type              string     `mapstructure:"type" pg:"-" json:"type,omitempty"`
+
+		DeviceTime        string  `mapstructure:"deviceTime" pg:"device_time,type:timestamptz" json:"deviceTime,omitempty"`
+
+		DeviceId          string   `mapstructure:"deviceId,omitempty" pg:"device_id" json:"deviceId,omitempty"`
+		Id                string   `mapstructure:"id,omitempty" pg:"id" json:"id,omitempty"`
+		Guid              string     `mapstructure:"guid,omitempty" pg:"guid" json:"guid,omitempty"`
+
+
+		Timezone          string   `mapstructure:"timezone,omitempty" pg:"timezone" json:"timezone,omitempty"`
+		TimezoneOffset    int64    `mapstructure:"timezoneOffset,omitempty" pg:"timezone_offset" json:"timezoneOffset,omitempty"`
+		ClockDriftOffset  int64    `mapstructure:"clockDriftOffset,omitempty" pg:"clock_drift_offset" json:"clockDriftOffset,omitempty"`
+		ConversionOffset  int64    `mapstructure:"conversionOffset,omitempty" pg:"conversion_offset" json:"conversionOffset,omitempty"`
+
+		UploadId          string   `mapstructure:"uploadId,omitempty" pg:"upload_id" json:"uploadId,omitempty"`
+
+		PayloadJson       string     `pg:"payload" json:"payload"`
+
+		Revision          int64   `mapstructure:"revision,omitempty" pg:"revision" json:"revision"`
+
+	}{
+		Time: b.Time,
+		Type: b.Type,
+		DeviceTime: strings.Trim(b.DeviceTime.Format(time.RFC3339), "Z"),
+		DeviceId: b.DeviceId,
+		Id: b.Id,
+		Guid: b.Guid,
+		Timezone: b.Timezone,
+		TimezoneOffset: b.TimezoneOffset,
+		ClockDriftOffset: b.ClockDriftOffset,
+		ConversionOffset: b.ConversionOffset,
+		UploadId: b.UploadId,
+		PayloadJson: b.PayloadJson,
+		Revision: b.Revision,
+	})
+}
