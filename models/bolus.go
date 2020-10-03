4f595519f7f45910079df8e813dd4fdeb364a608
James Raby <james.raby@gmail.com>
2020-10-03 14:37:56 -0700

    added fields to bolus record

--- models/bolus.go
+++ models/bolus.go
@@ -12,6 +12,13 @@ type Bolus struct {
 	Base                    `mapstructure:",squash"`
 
 	Normal         float64   `mapstructure:"normal" pg:"normal" json:"normal,omitempty"`
+	ExpectedNormal         float64   `mapstructure:"expectedNormal" pg:"expected_normal" json:"expectedNormal,omitempty"`
+
+	Duration                 float64   `mapstructure:"duration" pg:"duration" json:"duration,omitempty"`
+	ExpectedDuration         float64   `mapstructure:"expectedDuration" pg:"expected_duration" json:"expectedDuration,omitempty"`
+
+	Extended                 float64   `mapstructure:"extended" pg:"extended" json:"extended,omitempty"`
+	ExpectedExtended         float64   `mapstructure:"expectedExtended" pg:"expected_extended" json:"expectedExtended,omitempty"`
 
 	SubType        string    `mapstructure:"subType" pg:"sub_type" json:"subType,omitempty"`
 }
